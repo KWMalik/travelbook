59d281d9827ce5ef9af0591ec90131e1bb2671c6
Derek Hammer <derek.r.hammer@gmail.com>
2012-06-17 15:51:35 -0500

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travelbook::Application.config.secret_token = '44283d6fb0203b7bebeaf651b0e7e5bf8f4a6c4c9bd0d67b2871ecfa0ece4c9ab1c5394c09dcfa2e56d653960e9c0e4d5f7e7d763ba024e05900499bdf5e4140'
